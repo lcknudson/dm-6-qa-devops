6caee24a0f92452d14313055a8ce2395d0e220da
Lorin Knudson <lcknudson@gmail.com>
2023-07-28 09:03:19 -0600

    part 1 complete

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 // Add up the total health of all the robots
 const calculateTotalHealth = (robots) =>
   robots.reduce((total, { health }) => total + health, 0);
